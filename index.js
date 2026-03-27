a78fc2840c9b01d61287360f4ca61c9b7b8e54cb
“[bridgettenawari]” <[chebetbri@gmail.com]>
2026-03-27 16:35:05 +0300

    Login tracker

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
+function createLoginTracker(userInfo = {
+  "username": "user1",
+  "password": "password123"
+}) {
+  let attemptCount = 0;
+
+  const loginAttempt = (passwordAttempt) => {
+    attemptCount++;
+    if (passwordAttempt === userInfo.password) {
+      return "Login successful"
+    }
+    if (passwordAttempt !== userInfo.password && attemptCount <= 3) {
+      return `Attempt ${attemptCount}: Login failed`
+    }
+    else if (passwordAttempt === userInfo.password) {
+      return "Login successful"
+    }
+    else if (passwordAttempt !== userInfo.password && attemptCount <= 3) {
+      return `Attempt ${attemptCount}: Login failed`
+    }
+    else if (passwordAttempt === userInfo.password) {
+      return "Login successful"
+    }
+    else if (passwordAttempt !== userInfo.password && attemptCount <= 3) {
+      return `Attempt ${attemptCount}: Login failed`
+    }
+    if (attemptCount > 3) {
+      return "Account locked due to too many failed login attempts"
+    }
+  }
+  return loginAttempt;
+
+}
 
 
 
